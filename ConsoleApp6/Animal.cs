7321ae0ed7273512e94f90a41a5802f52b9603ab
lalad <lalad@DNJ>
2022-08-07 16:11:49 +0100

    Add project files.

---
+++ ConsoleApp6/Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Animal
+    {
+        public string Name { get; set; }
+
+        public string Communicates { get; set; }
+
+        public double Eating(string food)
+        {
+            return 54.0d;
+        }
+       
+    }
+
+    class dog : Animal
+    {
+
+    }
+
+    internal class SimpleInterest
+    {
+        public double principal;
+        public double rate { get; set; } = 5.0d;
+        public int time;
+
+        public double SI()
+        {
+            return principal * rate * time / 100;
+        }
+        public double Compound(CompoundInterestObj ciObj)
+        {
+            var amount = ciObj.Prin * ciObj.Rate / 100;
+        }
+    }
+
+    class CompoundInterestObj
+    {
+        public decimal Prin { get; set; } 
+        public decimal Rate { get; set; }
+        public double Inflation { get; set; }
+
+        public int Time { get; set; } 
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Access_Modifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            car honda = new car();
+            honda.color = "red";
+            honda.make = "atauu";
+            honda.Isdriver = true;
+            honda.Isfuel = true;
+            honda.IscarKey = true;
+            honda.Ispolicepresent = true;
+            Console.WriteLine(honda.Carstart());
+            Console.WriteLine(honda.Arrest());
+
+
+        }
+    }
+}
+
+
+namespace Access_Modifier
+{
+    public class car
+    {
+        public string color { get; set; }
+        public string Model { get; set; }
+        public string make { get; set; }
+        public bool Isautomatic { get; set; }
+        public bool IscarKey { get; set; }
+        public bool Isdriver { get; set; }
+
+        public bool Isfuel { get; set; }
+
+        public bool Ispolicepresent { get; set; }
+
+
+
+        public string Carstart()
+        {
+            if (Isdriver && IscarKey && Isfuel)
+            {
+                return "Welcome let us start the car";
+            }
+            else
+            {
+                return "Sorry we cannot start the car";
+            }
+        }
+
+        public string Arrest()
+        {
+            if (Ispolicepresent && color == "Red")
+            {
+                Console.WriteLine ("Carry On");
+            }
+            else
+            {
+                Console.WriteLine("Oga, Stop and Park there");
+            }
+        }
+
+    }
+}
